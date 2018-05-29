7d320511ae9e31fd914b2ceba90bde38239f4788
Alexina Chong <alexina.chong@gmail.com>
2018-05-28 17:23:38 -0700

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  
+  ReactDOM.render(<App />, root);
+  registerServiceWorker();
+});
